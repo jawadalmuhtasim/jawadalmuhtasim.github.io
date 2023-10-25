719b28a28b81f3e1da055ecb1f26479f50a799ee
Jawad Al Muhtasim <91711273+jawadalmuhtasim@users.noreply.github.com>
2023-10-25 21:55:58 +0600

    added a bug

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Programmer"],
+        strings: ["Student", "Programmer", "Passionate Communicator", "Stoic"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Programmer"],
+        strings: ["Student", "Programmer", "Passionate Communicator", "Stoic"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
